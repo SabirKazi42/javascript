3005f93b469e0bb02dd0a44dffa911b35e733193
sabirkazi42 <sabirkazi442@gmail.com>
2024-12-13 21:59:26 +0530

    commit

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-const students = [
-  { name: "Alice", grade: "A" },
-  { name: "Bob", grade: "B" },
-  { name: "Charlie", grade: "A" },
-  { name: "David", grade: "C" },
-  { name: "Eve", grade: "B" },
-];
-
-const r = students.reduce((rs, student) => {
-  if (rs[student.grade]) {
-    rs[student.grade].push(student.name);
-  } else {
-    rs[student.grade] = [student.name];
-  }
-  return rs;
-}, {});
-
+const r = new Date();
 console.log(r);
