22bc9b430978005e0bced7fd9ee00f04340f3f31
sabirkazi42 <sabirkazi442@gmail.com>
2024-12-13 19:50:02 +0530

    commit

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log("Hello World");
-console.log("Hello World");
+const sales = [
+  { product: "Laptop", quantity: 2, price: 1200 },
+  { product: "Phone", quantity: 5, price: 800 },
+  { product: "Tablet", quantity: 3, price: 600 },
+];
+
+const r = sales.reduce((start, sale) => {
+  return start + sale.price;
+}, 0);
+
+console.log(`Hello ${r}`);
